a1f9951fa16515374e7f2cdba6248e6e622b455e
Susan Cheng <susan.doggie@gmail.com>
2025-10-04 11:43:28 +0800

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -53,7 +53,7 @@ export default [
         entryFileNames: '[name].mjs',
         chunkFileNames: 'internals/[name]-[hash].mjs',
         dir: './dist',
-        format: 'esm',
+        format: 'es',
         sourcemap: true,
       },
     ],
